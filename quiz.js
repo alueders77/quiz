aee48e9593a8e396cfc7d66375be16615b2676eb
alueders77 <alueders77@gmail.com>
2014-05-19 10:41:11 -0500

    The first commit

---
+++ quiz.js
@@ -0,0 +1,332 @@
+var questiongrid = new Array (
+"<section class=\"question\" id=\"question0\"><div class=\"maincontent\"><p class=\"questiontext\">In China, the presence of a magpie can bring what?</p></div></section>",
+"<section class=\"question\" id=\"question1\"><div class=\"maincontent\"><p class=\"questiontext\">Which of the following is considered a game bird?</p></div></section>",		
+"<section class=\"question\" id=\"question2\"><div class=\"maincontent\"><p class=\"questiontext\">What invention greatly advanced the hunting of birds?</p></div></section>",
+"<section class=\"question\" id=\"question3\"><div class=\"maincontent\"><p class=\"questiontext\">Bird feathers were used to make what?</p></div></section>",
+"<section class=\"question\" id=\"question4\"><div class=\"maincontent\"><p class=\"questiontext\">DDT, a harmful pesticide, caused which bird population to decrease in large numbers?</p></div></section>",								
+"<section class=\"question\" id=\"question5\"><div class=\"maincontent\"><p class=\"questiontext\">What is the fastest growing hobby in the world?</p></div></section>",					
+"<section class=\"question\" id=\"question6\"><div class=\"maincontent\"><p class=\"questiontext\">Which one of these is not a daily threat to birds?</p></div></section>",				
+"<section class=\"question\" id=\"question7\"><div class=\"maincontent\"><p class=\"questiontext\">Which of the following characteristics is NOT unique to birds?</p></div></section>",
+"<section class=\"question\" id=\"question8\"><div class=\"maincontent\"><p class=\"questiontext\">What is one reason a bird might sing?</p></div></section>",		
+"<section class=\"question\" id=\"question9\"><div class=\"maincontent\"><p class=\"questiontext\">Migration is...</p></div></section>",
+"<section class=\"question\" id=\"question10\"><div class=\"maincontent\"><p class=\"questiontext\">How many birds are threatened or endangered in Nebraska?</p></div></section>",
+"<section class=\"question\" id=\"question11\"><div class=\"maincontent\"><p class=\"questiontext\">How does the Wild Bird Conservation Act protect birds?</p></div></section>",
+"<section class=\"question\" id=\"question12\"><div class=\"maincontent\"><p class=\"questiontext\">Which law states that it is unlawful to take any migratory bird, except non-native species?</p></div></section>",
+"<section class=\"question\" id=\"question13\"><div class=\"maincontent\"><p class=\"questiontext\">All of the following are ways you can help birds EXCEPT for what?</p></div></section>",
+"<section class=\"question\" id=\"question14\"><div class=\"sideimage\"><img src=\"./images/sideimage1.jpg\"/></div><div class=\"maincontent\"><p class=\"questiontext\">Which letter corresponds with the shaft?</p></div></section>",
+"<section class=\"question\" id=\"question15\"><div class=\"sideimage\"><img src=\"./images/sideimage2.jpg\"/></div><div class=\"maincontent\"><p class=\"questiontext\">Which one of these birds has a tearing beak?</p></div></section>",
+"<section class=\"question\" id=\"question16\"><div class=\"sideimage\"><img src=\"./images/sideimage3.jpg\"/></div><div class=\"maincontent\"><p class=\"questiontext\">Which one of these birds has perching feet?</p></div></section>",		
+"<section class=\"question\" id=\"question17\"><div class=\"sideimage\"><img src=\"./images/sideimage4.jpg\"/></div><div class=\"maincontent\"><p class=\"questiontext\">Which of these birds probes with its beak to find food?</p></div></section>",
+"<section class=\"question\" id=\"question18\"><div class=\"maincontent\"><p class=\"questiontext\">Interior Least Terns prefer to nest in what kind of habitat?</p></div></section>",
+"<section class=\"question\" id=\"question19\"><div class=\"maincontent\"><p class=\"questiontext\">Mountain Plovers benefit from which of these mammals?</p></div></section>",				
+"<section class=\"question\" id=\"question20\"><div class=\"maincontent\"><p class=\"questiontext\">Which one of these birds was affected by the extinction of the Rocky Mountain grasshopper?</p></div></section>",	
+"<section class=\"question\" id=\"question21\"><div class=\"maincontent\"><p class=\"questiontext\">Which bird can attain speeds in excess of 150 miles per hour as they attack their prey?</p></div></section>",																	
+"<section class=\"question\" id=\"question22\"><div class=\"maincontent\"><p class=\"questiontext\">Why does a Piping Plover pretend to have a broken wing when a predator is nearby?</p></div></section>",
+"<section class=\"question\" id=\"question23\"><div class=\"maincontent\"><p class=\"questiontext\">Young Whooping Cranes that have been reintroduced to the wild migrate between Wisconsin and Florida. They learn how to fly using which device?</p></div></section>",
+"<section class=\"question\" id=\"question24\"><div class=\"maincontent\"><p class=\"questiontext\">Plants that grow underwater with a few floating or emergent leaves are a great food source for diving ducks. In what part of a wetland habitat can you find these plants?</p></div></section>",
+"<section class=\"question\" id=\"question25\"><div class=\"sideimage\"><img src=\"./images/sideimage5.jpg\"/></div><div class=\"maincontent\"><p class=\"questiontext\">What bird is pictured in this photo?</p></div></section>",		
+"<section class=\"question\" id=\"question26\"><div class=\"sideimage\"><img src=\"./images/sideimage6.jpg\"/></div><div class=\"maincontent\"><p class=\"questiontext\">What bird is pictured in this photo?</p></div></section>",			
+"<section class=\"question\" id=\"question27\"><div class=\"sideimage\"><img src=\"./images/sideimage7.jpg\"/></div><div class=\"maincontent\"><p class=\"questiontext\">What bird is pictured in this photo?</p></div></section>",
+"<section class=\"question\" id=\"question28\"><div class=\"sideimage\"><img src=\"./images/sideimage8.jpg\"/></div><div class=\"maincontent\"><p class=\"questiontext\">What bird is pictured in this photo?</p></div></section>",				
+"<section class=\"question\" id=\"question29\"><div class=\"sideimage\"><img src=\"./images/sideimage9.jpg\"/></div><div class=\"maincontent\"><p class=\"questiontext\">What bird is pictured in this photo?</p></div></section>",	
+"<section class=\"question\" id=\"question30\"><div class=\"sideimage\"><img src=\"./images/sideimage10.jpg\"/></div><div class=\"maincontent\"><p class=\"questiontext\">What bird is pictured in this photo?</p></div></section>",	
+"<section class=\"question\" id=\"question31\"><div class=\"sideimage\"><img src=\"./images/sideimage11.jpg\"/></div><div class=\"maincontent\"><p class=\"questiontext\">What bird is pictured in this photo?</p></div></section>",
+"<section class=\"question\" id=\"question32\"><div class=\"sideimage\"><img src=\"./images/sideimage12.jpg\"/></div><div class=\"maincontent\"><p class=\"questiontext\">What bird is pictured in this photo?</p></div></section>"						
+						
+);
+
+var answergrid = new Array (
+["<div class=\"choicebtn rightone\">happiness</div",	
+"<div class=\"choicebtn\">rain showers</div	",
+"<div class=\"choicebtn\">bad luck</div",	
+"<div class=\"choicebtn\">winter storm</div"],
+
+["<div class=\"choicebtn rightone\">Wild Turkey</div	",	
+"<div class=\"choicebtn\">Bald Eagle</div	",
+"<div class=\"choicebtn\">American Robin</div",	
+"<div class=\"choicebtn\">American Goldfinch</div"],
+
+["<div class=\"choicebtn rightone\">Wildgun</div",	
+"<div class=\"choicebtn\">car</div",
+"<div class=\"choicebtn\">television</div",	
+"<div class=\"choicebtn\">computer</div"],
+
+["<div class=\"choicebtn rightone\">hats</div",	
+"<div class=\"choicebtn\">shoes</div	",
+"<div class=\"choicebtn\">furniture</div",	
+"<div class=\"choicebtn\">dresses</div"],
+
+["<div class=\"choicebtn rightone\">Bald Eagle</div",	
+"<div class=\"choicebtn\">Eastern Meadowlark</div	",
+"<div class=\"choicebtn\">Common Grackle</div",	
+"<div class=\"choicebtn\">Canada Goose</div"],
+
+["<div class=\"choicebtn rightone\">birding</div",	
+"<div class=\"choicebtn\">golf</div	",
+"<div class=\"choicebtn\">scuba diving</div",	
+"<div class=\"choicebtn\">hunting</div"],	
+
+["<div class=\"choicebtn rightone\">floods</div",	
+"<div class=\"choicebtn\">habitat loss</div	",
+"<div class=\"choicebtn\">cats on the loose</div",	
+"<div class=\"choicebtn\">poisons</div"],
+
+["<div class=\"choicebtn rightone\">ability to fly</div",	
+"<div class=\"choicebtn\">hollow bones</div	",
+"<div class=\"choicebtn\">feathers</div",	
+"<div class=\"choicebtn\">fused bones</div"],
+
+["<div class=\"choicebtn rightone\">to establish and defend a territory</div",	
+"<div class=\"choicebtn\">to become famous</div	",
+"<div class=\"choicebtn\">to help baby birds go to sleep</div",	
+"<div class=\"choicebtn\">to get the attention of its prey</div"],			
+
+["<div class=\"choicebtn rightone wide\">long flights between winter and breeding areas</div",	
+"<div class=\"choicebtn\">sleeping all winter long</div	",
+"<div class=\"choicebtn\">finding a good nest site</div",	
+"<div class=\"choicebtn wide\">flying from tree to tree in search of food</div"],
+
+["<div class=\"choicebtn rightone\">four</div",	
+"<div class=\"choicebtn\">five</div	",
+"<div class=\"choicebtn\">seven</div",	
+"<div class=\"choicebtn\">ten</div"],
+
+["<div class=\"choicebtn rightone wide\">prohibits import or export of certain exotic birds</div",	
+"<div class=\"choicebtn wide\">prohibits the taking of bald eagles and golden eagles</div	",
+"<div class=\"choicebtn wide\">provides protection for birds in Nebraska</div",	
+"<div class=\"choicebtn wide\">makes it unlawful to transport illegal wildlife</div"],	
+
+["<div class=\"choicebtn rightone wide\">the International Migratory Bird Treaty Act</div",	
+"<div class=\"choicebtn\">the Lacey Act</div	",
+"<div class=\"choicebtn\">the Endangered Species Act</div",	
+"<div class=\"choicebtn wide\">the Neotropical Bird Migratory Conservation Act</div"],
+
+["<div class=\"choicebtn rightone\">let your cat run loose</div",	
+"<div class=\"choicebtn\">provide bird feeders</div	",
+"<div class=\"choicebtn\">build a birdhouse</div",	
+"<div class=\"choicebtn\">provide water for birds</div"],	
+
+["<div class=\"choicebtn rightone\">A</div",	
+"<div class=\"choicebtn\">B</div	",
+"<div class=\"choicebtn\">C</div",	
+"<div class=\"choicebtn\">D</div"],
+
+["<div class=\"choicebtn rightone\">Swainson's Hawk</div",	
+"<div class=\"choicebtn\">Northern Cardinal</div	",
+"<div class=\"choicebtn\">Baltimore Oriole</div",	
+"<div class=\"choicebtn\">Great Blue Heron</div"],
+
+["<div class=\"choicebtn rightone wide\">Black-Capped Chickadee</div",	
+"<div class=\"choicebtn\">Killdeer</div	",
+"<div class=\"choicebtn\">Barn Owl</div",	
+"<div class=\"choicebtn\">Downy Woodpecker</div"],
+
+["<div class=\"choicebtn rightone\">White-Faced Ibis</div",	
+"<div class=\"choicebtn\">Great Blue Heron</div	",
+"<div class=\"choicebtn\">Northern Shoveller</div",	
+"<div class=\"choicebtn wide\">Red-Winged Blackbird</div"],
+
+["<div class=\"choicebtn rightone\">sandbars in rivers</div",	
+"<div class=\"choicebtn\">prairies</div	",
+"<div class=\"choicebtn\">deciduous forests</div",	
+"<div class=\"choicebtn\">farmland</div"],	
+
+["<div class=\"choicebtn rightone\">prairie dogs</div",	
+"<div class=\"choicebtn\">coyotes</div	",
+"<div class=\"choicebtn\">badgers</div",	
+"<div class=\"choicebtn\">raccoons</div"],	
+
+["<div class=\"choicebtn rightone\">Eskimo Curlew</div",	
+"<div class=\"choicebtn\">Bald Eagle</div	",
+"<div class=\"choicebtn\">Piping Plover</div",	
+"<div class=\"choicebtn\">Peregrine Falcon</div"],	
+
+["<div class=\"choicebtn rightone\">Peregrine Falcon</div",	
+"<div class=\"choicebtn\">Whooping Crane</div	",
+"<div class=\"choicebtn\">Eskimo Curlew</div",	
+"<div class=\"choicebtn\">Mountain Plover</div"],
+
+["<div class=\"choicebtn rightone\">to take attention away from the nest</div",	
+"<div class=\"choicebtn\">to make predators laugh</div	",
+"<div class=\"choicebtn wide\">because their wing is fragile and they get hurt a lot</div",	
+"<div class=\"choicebtn\">to show predators how tough they are</div"],
+
+["<div class=\"choicebtn rightone\">airplanes and pilots</div",	
+"<div class=\"choicebtn\">television</div	",
+"<div class=\"choicebtn\">robots</div",	
+"<div class=\"choicebtn\">mom and dad</div"],
+
+["<div class=\"choicebtn rightone\">submerged</div",	
+"<div class=\"choicebtn\">floating</div	",
+"<div class=\"choicebtn\">emergent</div",	
+"<div class=\"choicebtn\">shoreline</div"],
+
+["<div class=\"choicebtn rightone\">Eastern Meadowlark</div",	
+"<div class=\"choicebtn\">Killdeer</div	",
+"<div class=\"choicebtn\">American Robin</div",	
+"<div class=\"choicebtn wide\">Red-Winged Blackbird</div"],
+
+["<div class=\"choicebtn rightone\">Interior Least Tern</div",	
+"<div class=\"choicebtn\">Dickcissel</div	",
+"<div class=\"choicebtn\">Killdeer</div",	
+"<div class=\"choicebtn\">Northern Cardinal</div"],
+
+["<div class=\"choicebtn rightone wide\">Red-Winged Blackbird</div",	
+"<div class=\"choicebtn\">Eastern Meadowlark</div	",
+"<div class=\"choicebtn\">Dickcissel</div",	
+"<div class=\"choicebtn\">American Robin</div"],
+
+["<div class=\"choicebtn rightone\">Dickcissel</div",	
+"<div class=\"choicebtn\">Eastern Meadowlark</div	",
+"<div class=\"choicebtn\">Barn Owl</div",	
+"<div class=\"choicebtn\">Interior Least Tern</div"],
+
+["<div class=\"choicebtn rightone\">Killdeer</div",	
+"<div class=\"choicebtn\">Interior Least Tern</div	",
+"<div class=\"choicebtn\">Barn Owl</div",	
+"<div class=\"choicebtn\">Dickcissel</div"],
+
+["<div class=\"choicebtn rightone\">Barn Owl</div",	
+"<div class=\"choicebtn\">Interior Least Tern</div	",
+"<div class=\"choicebtn\">Dickcissel</div",	
+"<div class=\"choicebtn\">Killdeer</div"],
+
+["<div class=\"choicebtn rightone\">American Robin</div",	
+"<div class=\"choicebtn wide\">Red-Winged Blackbird</div	",
+"<div class=\"choicebtn\">Eastern Meadowlark</div",	
+"<div class=\"choicebtn\">Northern Cardinal</div"],
+
+["<div class=\"choicebtn rightone\">Northern Cardinal</div",	
+"<div class=\"choicebtn\">Eastern Meadowlark</div	",
+"<div class=\"choicebtn\">American Robin</div",	
+"<div class=\"choicebtn\">Dickcissel</div"]									
+);
+
+function shuffle(array) {
+  var currentIndex = array.length
+    , temporaryValue
+    , randomIndex
+    ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+var score = 0;
+var currentscore = 0;
+var questioncount = 0;
+shuffle(questiongrid);
+var backgroundnum = Math.floor(Math.random() * 20) + 1;
+$('#quizcontainer').append(questiongrid[0]);
+$('#quizcontainer').css("background-image", "url('./images/background" + backgroundnum + ".jpg')");
+var questionid = $(".question").attr('id');
+var questionnum = questionid.replace( /^\D+/g, '');
+shuffle(answergrid[questionnum]);
+$('.maincontent').append(answergrid[questionnum]);
+var wrongcount = 0;
+
+// What happens when you click on one of the answers.
+$(document.body).on("click", ".choicebtn", function choiceclick() {
+// If you hit the current answer...
+if ($(this).hasClass('rightone')) {
+$('#quizcontainer').prepend("<div id='clickblock'></div>");
+// Assigns points based on how many wrong answers you've already clicked/
+if (wrongcount == 0) {
+currentscore = 100;
+score+=100;
+}
+if (wrongcount == 1) {
+currentscore = 50;
+score+=50;
+}
+if (wrongcount == 2) {
+currentscore = 25;
+score+=25;
+}
+if (wrongcount > 2) {
+currentscore = 0;
+score+=0;
+}
+// Resets the wrong answer counter, and increases the number of questions answered by 1.
+wrongcount = 0;
+questioncount++;
+// Changes the right answer buttons color green, add displays the number of points scored.
+$(this).addClass('right');
+$(this).append("<p class='rightnumber'>+" + currentscore + "</p>");
+$('.rightnumber').fadeIn();
+// What happens when all ten questions are answered
+if (questioncount == 10) {
+var rating = score / 10;
+var ratingtext;
+if (rating >= 0) {
+		ratingtext = "dodo";
+	}
+	if (rating >= 15) {
+		ratingtext = "cold turkey";
+	}
+	if (rating >= 30) {
+		ratingtext = "wet hen";
+	}
+	if (rating >= 45) {
+		ratingtext = "silly goose";
+	}
+	if (rating >= 60) {
+		ratingtext = "early bird";
+	}
+	if (rating >= 70) {
+		ratingtext = "rockin' robin";
+	}
+	if (rating >= 80) {
+		ratingtext = "eagle eye";
+	}
+	if (rating >= 90) {
+		ratingtext = "wise owl";
+	}
+	if (rating == 100) {
+		ratingtext = "egghead";
+	}
+setTimeout(function(){
+$('.question').remove();
+$('#pointsdisplay').text(score);
+$('#ratingdisplay').text(ratingtext);
+$('#tryagain').click(function() {
+location.reload();	
+});
+$('#winscreen').fadeIn();
+}, 1000);
+
+}
+// What happens if  there are still questions left to be answered.
+else {
+setTimeout(function(){$('.question').remove();
+$('#quizcontainer').append(questiongrid[questioncount]);
+backgroundnum = Math.floor(Math.random() * 20) + 1;
+$('#quizcontainer').css("background-image", "url('./images/background" + backgroundnum + ".jpg')");
+questionid = $(".question").attr('id');
+questionnum = questionid.replace( /^\D+/g, '');
+shuffle(answergrid[questionnum]);
+$('.maincontent').append(answergrid[questionnum]);
+$("#clickblock").remove();
+}, 1000);
+}
+}
+// If a wrong answer is clicked...
+else {
+wrongcount++;
+$(this).append("<img class='wrongimage' src='./images/redx.png'/>");
+$(this).addClass('wrong');
+setTimeout(function(){$('.wrong').fadeOut();}, 250);
+}
+	
+});
